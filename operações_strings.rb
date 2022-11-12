66dcb8a925173a657794515079289c2024c7daf6
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-12 11:55:28 -0300

    Create operações_strings.rb

---
+++ operações_strings.rb
@@ -0,0 +1,15 @@
+ nome = "Cadu"
+ p nome
+ p nome[0]
+ p nome [nome.length-1]
+ p nome [-1]
+ p nome [nome.length-2]
+ p nome.chars
+ p nome.length
+ p nome[0,3]
+
+ p "Cabeçalho"
+ p "-"*10
+ p "testo de informação"
+
+ 
