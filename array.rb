e4e0dfcaa1e8608e47cfbe5bd3b41ae213de5863
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-12 15:21:42 -0300

    Update array.rb

--- array.rb
+++ array.rb
@@ -1,6 +1,17 @@
 lista = []
 lista.append(40)
+lista.append("50")
 lista.append("string")
 lista.append(true)
 p lista
 p lista.length
+p lista.first
+p lista.last
+
+#Ajuntando listas
+puts "\n"
+lista_1 = [1,2,3]
+lista_2 = [4,5,6]
+p lista_1
+p lista_2
+p lista_1 + lista_2
