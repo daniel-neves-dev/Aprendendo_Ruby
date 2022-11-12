764cc22a8f59a62555034c93dd73b66c16b2866b
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-12 11:14:04 -0300

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -2,9 +2,11 @@ puts "Hello everyone!!!"
 nome = "Cadu"
 
 mensagem = <<~MENSAGEM
-    olá #{nome}
+    \nolá #{nome}
     Bem vindo(a) ao  meu programa!
     Obrigado.
 MENSAGEM
 
 puts mensagem
+
+puts "\nO tamanho da minha string é: #{mensagem.length()}"
