6a70fdf47e3770e52752add35ece7b5c1e10ba96
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-13 12:00:30 -0300

    Create laços.rb

---
+++ laços.rb
@@ -0,0 +1,19 @@
+#WHILE continuação
+#valor = 0
+#while valor < 10
+#    p "O valor é #{valor}"
+#    valor += 1
+#end
+
+#BREAK, para a condição
+
+#while valor < 10
+#    p "O novo valor é #{valor}"
+#    break if valor == 5
+#    valor +=1
+#end
+
+#Repetição FOR
+for numero in [1,2,3,4,5,6,7,8,9,10]
+    p "O núemro é #{numero}"
+end
