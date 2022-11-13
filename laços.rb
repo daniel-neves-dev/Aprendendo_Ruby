517f23be56c0866cf618a251ea6ef4ce90b43ca2
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-13 12:09:28 -0300

    Update laços.rb

--- laços.rb
+++ laços.rb
@@ -14,6 +14,25 @@
 #end
 
 #Repetição FOR
-for numero in [1,2,3,4,5,6,7,8,9,10]
-    p "O núemro é #{numero}"
+#for numero in [1,2,3,4,5,6,7,8,9,10]
+#    p "O núemro é #{numero}"
+#end
+
+#Repetição FOR usando RANGE
+#range = 1..8
+#for n in range
+#    p "O range esta em #{n}"
+#end
+
+#metodo EACH
+lista = [1,2,3,4,5]
+
+lista.each do |valor|#variavel
+    p "Número na lista (usando 'each') #{valor}"
+end
+
+puts"\n"
+
+for valor in lista
+    p "Número na lista (usando 'for') #{valor}"
 end
