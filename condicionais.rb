e65f5ea8a48e5cdec4928da7d638a1fc40dbeb46
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-13 11:41:38 -0300

    Update condicionais.rb

--- condicionais.rb
+++ condicionais.rb
@@ -8,12 +8,24 @@
 #    p "Esse valor é menor do que 20"
 #end
 
+#puts "\n"
+#p "Digite um número"
+#numero = gets.chomp.to_i
+#if numero >= 50 && numero <= 100
+#    p "O número esta entre 50 e 100"
+#elsif numero <50
+#    p "O núemro esta abaixo de 50"
+#else 
+#    p "O número esta acima de 100"
+#end
+
+puts "\n"
+#UNLESS só executa quando o valor for falso, exemplo
 p "Digite um número"
-numero = gets.chomp.to_i
-if numero >= 50 && numero <= 100
-    p "O número esta entre 50 e 100"
-elsif numero <50
-    p "O núemro esta abaixo de 50"
-else 
-    p "O número esta acima de 100"
+n_numero = gets.chomp.to_i
+unless n_numero == 100
+    p "Este número não é o valor 100"
+else
+    p "Número correto"
 end
+
