0edc64fb19ea82f1f80347b777633833bec418ca
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-14 15:45:34 -0300

    Update exercicio_palindromo.rb

--- exercicio_palindromo.rb
+++ exercicio_palindromo.rb
@@ -1,19 +1,23 @@
+#def palindromo?(palavra)
+#    palavra = palavra.downcase
+#    palavra_reversa = ""
+
+#    indice = palavra.length
+
+#    until indice == 0
+#        palavra_reversa += palavra[indice-1]
+#        indice -= 1  
+#   end
+
+#    if palavra_reversa == palavra
+#        true
+#    else
+#        false
+#    end
+#end
+
 def palindromo?(palavra)
-    palavra = palavra.downcase
-    palavra_reversa = ""
-
-    indice = palavra.length
-
-    until indice == 0
-        palavra_reversa += palavra[indice-1]
-        indice -= 1  
-    end
-
-    if palavra_reversa == palavra
-        true
-    else
-        false
-    end
+    palavra.downcase == palavra.downcase.reverse
 end
 
 p palindromo?("ovo")
