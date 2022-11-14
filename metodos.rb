ab0452e7760034e04ad56716e0218fcb0b7b372d
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-14 14:55:47 -0300

    Update metodos.rb

--- metodos.rb
+++ metodos.rb
@@ -7,10 +7,11 @@
 #p soma(10,5)
 
 #PARAMETROS NOMEADOS
-def soma_parametros_nomeados(n1:, n2:)
+def soma_parametros_nomeados(n1:, n2:0)
     p "Somar #{n1} e #{n2}"
     n1+n2
 end
 p "Execuando soma..."
 p soma_parametros_nomeados(n1:3, n2:6)
 p soma_parametros_nomeados(n2:6, n1:3)
+p soma_parametros_nomeados(n1:10)
