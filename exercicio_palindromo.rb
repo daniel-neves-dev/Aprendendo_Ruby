de4173135906dc2bcd346357c2e83359fe67c4ac
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-14 15:23:51 -0300

    Create exercicio_palindromo.rb

---
+++ exercicio_palindromo.rb
@@ -0,0 +1,21 @@
+def palindromo?(palavra)
+    palavra = palavra.downcase
+    palavra_reversa = ""
+
+    indice = palavra.length
+
+    until indice == 0
+        palavra_reversa += palavra[indice-1]
+        indice-=1
+    end
+
+    if palavra_reversa == palavra
+        true
+    else
+        false
+    end
+end
+
+p palindromo?("ovo")
+p palindromo?("Ovo")
+p palindromo?("carro")
